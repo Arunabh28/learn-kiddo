469ca72149f811af5aa3a6edcab0a8a63ec8e557
Arunabh28 <arunabh_28@hotmail.com>
2021-02-28 00:36:23 -0800

    Fixed mobile touch issue on canvas element.

--- src/components/DrawingBoard.vue
+++ src/components/DrawingBoard.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-      
+    <div>      
       <canvas id="canvas" v-on:mousedown="handleMouseDown" 
       v-on:mouseup="handleMouseUp" v-on:mousemove="handleMouseMove" 
       v-bind:width="canvasWidth" v-bind:height="canvasHeight">        
@@ -12,23 +11,34 @@
         <button class="btn btn-sm mr-1" style="background-color:#fce703;color:white;" @click="UpdatePenColor('#fce703')"></button>        
         <button class="btn btn-sm mr-1" style="background-color:#000000;color:white;" @click="UpdatePenColor('#000000')"></button>        
         <button class="btn btn-sm pull-right text-danger" @click="ResetCanvas()">X</button>
-      </div>
-    </div>
-        
+      </div>      
+    </div>        
 </template>
+
 <script>
     export default{
         name:"drawing-board",
         props:{
             canvasWidth:Number,
             canvasHeight:Number,
-            showNumber:Boolean
-            
+            showNumber:Boolean            
         },
         data:function(){
             return{
               penColor:String,
+              canvasElement:HTMLCanvasElement,
                 mouse:{
+                    current: {
+                    x: 0,
+                    y: 0
+                    },
+                previous: {
+                    x: 0,
+                    y: 0
+                },
+                down: false
+                },
+                touch:{
                     current: {
                     x: 0,
                     y: 0
@@ -41,17 +51,42 @@
                 }
             }
         },
+        created:function(){
+          this.canvasElement= document.getElementById("canvas");
+        },
         mounted:function(){
+          this.canvasElement= document.getElementById("canvas");
            this.ResetCanvas();
-            this.penColor="#F63E02";
+            this.penColor="#F63E02"; 
+            var self=this;           
+            this.canvasElement.ontouchstart=function(event){
+              self.touchstarted(event);
+             event.stopPropagation();
+            };
+            this.canvasElement.ontouchmove=function(event){
+              self.ontouchmove(event);
+              event.stopPropagation();
+            },
+            this.canvasElement.ontouchend=function(event){
+              event.stopPropagation();
+              self.ontouchend(event);
+            }
         },
         computed:{
             currentMouse: function () {
-            var c = document.getElementById("canvas");
+            var c = this.canvasElement;
             var rect = c.getBoundingClientRect();            
             return {
                 x: this.mouse.current.x - rect.left,
                 y: this.mouse.current.y - rect.top
+                }
+            },
+            currentTouch: function () {
+            var c = this.canvasElement;
+            var rect = c.getBoundingClientRect();            
+            return {
+                x: this.touch.current.x - rect.left,
+                y: this.touch.current.y - rect.top
                 }
             }
         },
@@ -64,14 +99,9 @@
                 var x=i*spacing + 15
                 ctx.beginPath();	
                   ctx.arc(x, 50, 4, 0, 2 * Math.PI);
-                  ctx.stroke();
-                  
-                  ctx.fillText(i, x-5, 75);
-                  
+                  ctx.stroke();                  
+                  ctx.fillText(i, x-5, 75);                  
               }
-             
-
-              
           },
           UpdatePenColor:function(pen,event){
               this.penColor=pen;
@@ -80,7 +110,7 @@
               }
           },
           ResetCanvas:function(){
-            var c = document.getElementById("canvas");
+            var c = this.canvasElement;
            var ctx = c.getContext("2d");
            ctx.clearRect(0,0,this.canvasWidth,this.canvasHeight);
             if(this.showNumber)
@@ -90,44 +120,40 @@
             }
           },
             draw: function () {
-      
-      
-      // requestAnimationFrame(this.draw);
-     if (this.mouse.down ) {
-       var c = document.getElementById("canvas");
-
-    var ctx = c.getContext("2d");
-       
-       //ctx.clearRect(0,0,800,800);
-       
-       
-       
-  
-    ctx.lineTo(this.currentMouse.x, this.currentMouse.y);
-       ctx.strokeStyle =this.penColor;
-       ctx.lineWidth = 2;
-    ctx.stroke();
-    
-     }
-     
-    },
-    handleMouseDown: function (event) {
-      this.mouse.down = true;
-      this.mouse.current = {
-        x: event.pageX,
-        y: event.pageY
-      }
-
-            var c = document.getElementById("canvas");
-      var ctx = c.getContext("2d");
-
-      ctx.moveTo(this.currentMouse.x, this.currentMouse.y)
-      
-
-    },
-        handleMouseUp: function () {
-      this.mouse.down = false;
-    },
+              // requestAnimationFrame(this.draw); 
+              var x;
+              var y;
+              var c = this.canvasElement;
+              var ctx = c.getContext("2d");
+              if (this.mouse.down ) {
+                //ctx.clearRect(0,0,800,800);
+                x=this.currentMouse.x;
+                y=this.currentMouse.y;       
+              }
+              if (this.touch.down ) {
+                //ctx.clearRect(0,0,800,800);
+                x=this.currentTouch.x;
+                y=this.currentTouch.y;       
+              }
+              if(this.touch)
+              ctx.lineTo(x, y);
+              ctx.strokeStyle =this.penColor;
+              ctx.lineWidth = 2;
+              ctx.stroke();      
+            },
+            handleMouseDown: function (event) {
+              this.mouse.down = true;
+              this.mouse.current = {
+                x: event.pageX,
+                y: event.pageY
+              }
+              var c = this.canvasElement;
+              var ctx = c.getContext("2d");
+              ctx.moveTo(this.currentMouse.x, this.currentMouse.y)             
+            },
+          handleMouseUp: function () {
+              this.mouse.down = false;
+            },
     handleMouseMove: function (event) {
 
       this.mouse.current = {
@@ -137,6 +163,34 @@
       
       this.draw(event)
       
+    },
+    touchstarted:function(event){
+      //
+      this.touch.down=true;
+      //console.log(event);
+      this.touch.current = {
+        x: event.touches[0].clientX,
+        y: event.touches[0].clientY
+      }
+      var c = this.canvasElement;
+      var ctx = c.getContext("2d");
+      ctx.moveTo(this.currentTouch.x, this.currentTouch.y)
+      this.draw(event);
+      event.preventDefault();
+    },
+    ontouchmove:function(event){
+      
+      this.touch.current = {
+         x: event.touches[0].clientX,
+        y: event.touches[0].clientY
+      }
+      
+      this.draw(event)
+      event.preventDefault();
+    },
+    ontouchend:function(event){
+      this.touch.down=false;
+      event.preventDefault();
     }
   },
                   ready: function () {
